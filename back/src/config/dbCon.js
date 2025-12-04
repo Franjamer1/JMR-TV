75b28e23c47c7481bb15d72252c4863272857614
Francisco Patiño <franjamer1@gmail.com>
2025-12-04 18:38:21 -0300

    change mongoose.connect in dbCon.js

--- back/src/config/dbCon.js
+++ back/src/config/dbCon.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 
 const dbCon = async () => {
     //realizo la conexion con la BDD
-    await mongoose.connect("mongodb+srv://franjamer1:Gallinadonavidad1@henrym2.r9qg6og.mongodb.net/HenryM2?retryWrites=true&w=majority&appName=HenryM2");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Conexion a la base de datos exitosa");
-    // console.log(process.env.MONGO_URI);
+    console.log(process.env.MONGO_URI);
 }
 
 module.exports = dbCon;
