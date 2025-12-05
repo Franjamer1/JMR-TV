1a7817c35f9bdea306061e0605d7e0e5ea2dbf09
Francisco Patiño <franjamer1@gmail.com>
2025-12-05 00:16:21 -0300

    moviesServices changes

--- back/src/services/moviesServices.js
+++ back/src/services/moviesServices.js
@@ -3,20 +3,8 @@ const Movie = require("../models/Movies");
 const movies = require("../models/Movies")
 
 module.exports = {
-    // getMovies: () => {
-    //     return axios.get('https://students-api.up.railway.app/movies')
-    //         .then(response => {
-    //             // console.log('Fetched movies:', response.data);
-    //             return response.data;
-    //         })
-    //         .catch(error => {
-    //             return `Error fetching movies. ${error}`;
-    //         });
-    // }
+
     getMovies: async () => {
-        // const response = await axios.get('https://students-api.up.railway.app/movies');
-        // console.log('Fetched movies:', response.data);
-        // return response.data;
         const getMovies = await movies.find();
         return getMovies;
     },
