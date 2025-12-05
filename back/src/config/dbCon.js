1d210f7c399a3c419a6b8bff7413dc0916fb6703
Francisco Patiño <franjamer1@gmail.com>
2025-12-05 00:22:21 -0300

    log in dbCon

--- back/src/config/dbCon.js
+++ back/src/config/dbCon.js
@@ -5,6 +5,7 @@ const dbCon = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Conexión a MongoDB exitosa");
+        console.log("MONGO_URI desde Railway:", process.env.MONGO_URI);
     } catch (error) {
         console.error("Error al conectar con MongoDB:", error);
         throw error;
